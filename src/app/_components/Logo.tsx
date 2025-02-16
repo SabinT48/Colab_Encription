13553e000d4e26a06dff1a307033058509e824f9
Eravity <cebotaricristian92@gmail.com>
2025-02-16 19:03:23 +0200

    Update Logo size and placement

--- src/app/_components/Logo.tsx
+++ src/app/_components/Logo.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image'
 export default function Logo() {
   return (
     <div className='flex items-center space-x-1 px-4'>
-      <Image src={logo} width={30} height={30} alt='lock_icon' />
+      <Image src={logo} width={25} height={25} alt='lock_icon' className='mb-1' />
       <h1 className="text-2xl font-bold text-black tracking-wide">LockByte</h1>
     </div>
   );
